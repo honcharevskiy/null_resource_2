424fcbefe35d9601ab4c429e6606e5325efb7e3d
Nickolay Honcharevskyi <goncharevskiyy@gmail.com>
2021-08-12 16:53:06 +0300

    private plugins provider

--- main.tf
+++ main.tf
@@ -1,2 +1,16 @@
+terraform {
+  required_providers {
+    scalr = {
+      source = "private-registry-18285.herokuapp.com/scalr/scalr"
+      version = "1.0.0"
+    }
+  }
+}
+
+# provider scalr {
+ # hostname = var.hostname
+  #token    = var.api_token
+#}
+
 resource null_resource "monorepo" {
 }
