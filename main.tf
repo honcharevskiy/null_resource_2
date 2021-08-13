a2bd0371f6d9f2b0ef4fbab6490a22204fa88358
Nickolay Honcharevskyi <goncharevskiyy@gmail.com>
2021-08-13 10:08:06 +0300

    foo

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     scalr = {
-      source = "private-registry-18285.herokuapp.com/scalr/scalr"
+      source = "terraform-provider.herokuapp.com/scalr/scalr"
       version = "2.0.0"
     }
   }
