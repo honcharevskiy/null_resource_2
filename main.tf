d125c946519ab96b8ee4015955197f20d600183e
Nickolay Honcharevskyi <goncharevskiyy@gmail.com>
2021-08-16 11:43:25 +0300

    foor

--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
 terraform {
   required_providers {
     scalr = {
-      source = "terraform-provider.herokuapp.com/scalr/scalr"
+      source  = "terraform-provider.herokuapp.com/scalr/scalr"
       version = "2.1.0"
     }
-    1password = {
-      source = "milosbackonja/1password"
-      version = "1.1.0"
-   }
+    acme = {
+      source  = "vancluever/acme"
+      version = "2.5.3"
+    }
+
   }
 }
 
