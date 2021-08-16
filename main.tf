33c16628f5d81a398983b1b5a818c2dae05f00bb
Nickolay Honcharevskyi <goncharevskiyy@gmail.com>
2021-08-16 11:38:14 +0300

    foor

--- main.tf
+++ main.tf
@@ -4,17 +4,9 @@ terraform {
       source = "terraform-provider.herokuapp.com/scalr/scalr"
       version = "2.1.0"
     }
-    scalr2 = {
-      source = "terraform-provider.herokuapp.com/scalr/scalr2"
-      version = "2.0.0"
-    }
-    scalr3 = {
-      source = "terraform-provider.herokuapp.com/scalr/scalr3"
-      version = "2.1.0"
-    }
-    scalr4 = {
-      source = "terraform-provider.herokuapp.com/scalr/scalr4"
-      version = "1.0.0"
+   acp = {
+      source = "allence-tunisie/acp"
+      version = "0.0.0"
     }
   }
 }
