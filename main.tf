2f576d9992f60bfc914dda1930631f4c1d19a0ff
Nickolay Honcharevskyi <goncharevskiyy@gmail.com>
2021-08-25 11:12:27 +0300

    foo

--- main.tf
+++ main.tf
@@ -2,29 +2,8 @@ terraform {
   required_providers {
     scalr = {
       source  = "terraform-provider.herokuapp.com/scalr/scalr"
-      version = "1.0.0"
+      version = "2.0.0"
     }
-    acme = {
-      source  = "vancluever/acme"
-      version = "2.5.3"
-    }
-    ansible = {
-      source  = "nbering/ansible"
-      version = "1.0.4"
-    }
-    azuredevops = {
-      source  = "microsoft/azuredevops"
-      version = "0.1.6"
-    }
-    archive = {
-      source  = "safanaj/archive"
-      version = "2.0.1"
-    }
-    aws = {
-      source  = "maddijoyce/aws"
-      version = "3.15.1"
-    }
-
   }
 }
 
