7c7051c54a460e38d038f120e635a87fffb7418f
Nickolay Honcharevskyi <goncharevskiyy@gmail.com>
2021-09-06 11:06:21 +0300

    foo

--- main.tf
+++ main.tf
@@ -1,10 +1,24 @@
 terraform {
   required_providers {
-    scalr = {
-      source  = "terraform-provider.herokuapp.com/scalr/scalr"
-      version = "2.1.0"
+    null = {
+      source = "hashicorp/null"
+      version = "3.1.0"
     }
   }
 }
 
+resource "null_resource" "foo" {
 
+}
+
+variable "first_variable" {
+}
+
+locals {
+    first_variable = var.first_variable
+   
+}
+
+output "foo" {
+   value = var.first_variable
+}
