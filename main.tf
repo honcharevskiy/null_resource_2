d68c61bad05ccba13d89be22f687bdb43fa3fb28
Nickolay Honcharevskyi <goncharevskiyy@gmail.com>
2021-10-04 13:46:18 +0300

    use ubuntu image

--- main.tf
+++ main.tf
@@ -1,23 +1,32 @@
-terraform {
-  required_providers {
-    null = {
-      source = "hashicorp/null"
-      version = "3.1.0"
-    }
+provider "aws" {
+ region = var.region
+}
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
-
-
-  backend "remote" {
-    hostname = "c70d784fa5c8.test-env.scalr.com"
-    organization = "env-svrcnchebt61e30"
-
-    workspaces {
-      name = "new_cli"
-    }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"]
+}
+resource "aws_instance" "ubuntu" {
+  #count = 5002
+  #ami                         = var.win_ami
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type1
+  subnet_id     = var.subnet
+  tags          = merge({ "Name" = format("k.kotov-test -> %s -> %s", substr("🤔🤷", 0, 1), data.aws_ami.ubuntu.name) }, var.tags)
+  timeouts {
+    create = "9m"
+    delete = "15m"
   }
 }
 
-resource "null_resource" "foo" {
-    count = 5000
+output "sensitive" {
+  sensitive = true
+  value     = "VALUE"
 }
-
