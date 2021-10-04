f367a52f2a8ab7f0a399931202dd69ac9365a169
Nickolay Honcharevskyi <goncharevskiyy@gmail.com>
2021-10-04 15:36:01 +0300

    fix linter error

--- main.tf
+++ main.tf
@@ -6,14 +6,10 @@ resource "aws_instance" "ubuntu" {
   ami                         = var.win_ami
   instance_type = var.instance_type1
   subnet_id     = var.subnet
-tags          = merge({ "Name" = format("m.honcharevskyi -> %s, substr("🤔🤷", 0, 1)) }, var.tags)
+  tags          = merge({ "Name" = format("m.honcharevskyi -> %s", substr("🤔🤷", 0, 1)) }, var.tags)
   timeouts {
     create = "9m"
     delete = "15m"
   }
 }
 
-output "sensitive" {
-  sensitive = true
-  value     = "VALUE"
-}
