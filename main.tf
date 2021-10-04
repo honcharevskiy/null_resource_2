045d9107b8c85f550d80b69a66f98341d75f3b4d
Nickolay Honcharevskyi <goncharevskiyy@gmail.com>
2021-10-04 11:36:20 +0300

    use 5000 null resources

--- main.tf
+++ main.tf
@@ -5,6 +5,16 @@ terraform {
       version = "3.1.0"
     }
   }
+
+
+  backend "remote" {
+    hostname = "c70d784fa5c8.test-env.scalr.com"
+    organization = "env-svrcnchebt61e30"
+
+    workspaces {
+      name = "new_cli"
+    }
+  }
 }
 
 resource "null_resource" "foo" {
